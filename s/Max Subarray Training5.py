ca00e34d116fe4379ab0196e518e270a052e4042
Sekhara17 <kreddysekhar196@gmail.com>
2024-11-30 23:20:51 +0530

    comments

--- s/Max Subarray Training5.py
+++ s/Max Subarray Training5.py
@@ -1,9 +1,9 @@
-#We take an array
+# We take an array
 arr  = [1,4,5,6,7,3,2]
-#A variable to store Maximun sum
+# A variable to store Maximun sum
 max_sum = 0
 s = 0
-for i in arr:
+for i in arr:   # For loop to iterate the end of the arr
     s += i
     if max_sum > s:
         max_sum = s
